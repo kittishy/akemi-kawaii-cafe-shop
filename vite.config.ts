e04cad2177e806517c85b6d1f3b9de8b4691b505
solara.julia.a <solara.julia.a@gmail.com>
2025-05-08 16:27:05 +0000

    test deploy 1

--- vite.config.ts
+++ vite.config.ts
@@ -3,33 +3,14 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: 'handle-missing-modules',
-      resolveId(id) {
-        // Handle react-icons imports specifically
-        if (id.startsWith('react-icons/')) {
-          return { id: 'virtual:empty-module', external: false };
-        }
-        return null;
-      },
-      load(id) {
-        if (id === 'virtual:empty-module') {
-          // Return an empty module that exports a component that renders nothing
-          return `
-            import React from 'react';
-            export const FaTwitter = () => React.createElement('span', null, 'T');
-            export const FaFacebook = () => React.createElement('span', null, 'F');
-            export const FaWhatsapp = () => React.createElement('span', null, 'W');
-            export const FaLink = () => React.createElement('span', null, 'L');
-            export default { FaTwitter, FaFacebook, FaWhatsapp, FaLink };
-          `;
-        }
-        return null;
+  plugins: [react()],
+    build: {
+    rollupOptions: {
+      external: [
+        'react-icons/fa'
+      ]
       }
-    }
-  ],
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
