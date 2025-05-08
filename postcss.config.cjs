bc613c26ea3b24aafe7808dfaf0d0bdb8743dd3c
solara.julia.a <solara.julia.a@gmail.com>
2025-05-08 16:21:19 +0000

    more fixs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,23 +1,13 @@
 const { defineConfig } = require('vite')
 const path = require('path')
-const tailwindNesting = require('tailwindcss/nesting')
+const tailwindNesting = require('tailwindcss/nesting');
 const autoprefixer = require('autoprefixer')
 
 module.exports = defineConfig({
   plugins: [tailwindNesting(), autoprefixer()],
-    resolve: {
-        alias: {
+  resolve: {
+    alias: {
       '@': path.resolve(__dirname, './src'),
-        }
     },
-    build: {
-        rollupOptions: {
-            external: ['react-icons/fa'],
-            output: {
-                globals: {
-          'react-icons/fa': 'ReactIconsFa',
-                }
-            }
-        }
-    }
+  },
 })
